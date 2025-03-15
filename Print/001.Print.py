7998bf49cbcfc9b10ad3afcd37529f8a34203f71
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-15 15:07:47 +0530

    Print function in Python

---
+++ Print/001.Print.py
@@ -0,0 +1,33 @@
+# Task : Print Hello World in One Line : Hello World 
+
+# Solution 1 : 
+# print("Hello"," World")
+
+
+# condition : We have to use two print statements
+# print("Hello"),print("World")
+
+# \n - new line 
+# print("Hello" , end=' ') 
+# print("World", end="Challenge Completed")
+
+print("Hello", end="1")
+
+# Write a code which overwrites the default behaviour of "sep" parameter in print() function
+
+print("Hello World")
+
+# print("Arvinder"s phone"")
+
+print('Arvinder"s phone')
+
+print("Arvinder's Car")
+
+print('''Hello world! Arvinder's car , Arvinder"Phone ''')
+
+print("""Hello world! Arvinder's car , Arvinder"Phone """)
+
+"""Hello """
+# Doc string 
+'''Hello'''
+
