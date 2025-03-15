278dac448426ac5773621ce0d083622b88f0f7b5
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-15 15:24:04 +0530

    Data Types in Python

--- Variable/002.DataTypes.py
+++ Variable/002.DataTypes.py
@@ -1,3 +1,17 @@
 # Data types : 
 
 # Numerical -- Int , Float 
+
+x = 400
+y = 670.80
+print(type(x))
+print(type(y))
+# string  -- "" 
+name = "Govind"
+print(name)
+print(type(name))
+# Boolean - True False 
+flag = True
+NoFlag = False
+print(flag)
+print(type(flag))
