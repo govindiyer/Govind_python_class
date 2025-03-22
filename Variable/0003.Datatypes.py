e1dc8d6e837d1f696f01ae4a0f4c6c09101415ca
Arvinder Pal <54645908+Arvinderpal10@users.noreply.github.com>
2025-03-22 15:49:36 +0530

    Create 0003.Datatypes.py

---
+++ Variable/0003.Datatypes.py
@@ -0,0 +1,74 @@
+x = 10
+
+print(type(x))
+
+x = "Govind"
+print(type(x))
+
+# Python is a dynamically typed langauge -- 
+
+# Complex Numbers -- 
+z = 1 + 3j
+print(type(z))
+print(z)
+
+
+# Type conversion : 
+
+a = 40
+b = "40"
+print(a,b)
+print(a+10)
+# print(b+10)
+
+print(a+ int(b))
+
+y = 300.56
+c = 400.44
+print(y+c)
+
+print(int(y+c))
+
+v = 100
+print(v)
+
+print(float(v))
+
+print(5/2)    # complete division 
+print(5//2)   # integral division   
+print(int(5/2))
+
+
+# Int to float 
+x = 200
+print(str(x)+"Hello")
+print(float(x))
+
+# float to int 
+
+f = 100.25 
+print(int(f))
+
+print(int(f)*4)
+print(f*4)
+
+# Boolean 
+
+# True - 1 False - 0
+
+print(int(True))
+print(int(False))
+
+# True -- except 0 -- all are true 
+# False -  False , "" , 0 
+
+print(int(bool(100)))
+
+print(int(bool(100)) + int(bool(-100)))
+
+print(bool(-11000))
+print(bool(5//3))
+print(bool(0/5))
+t = 56
+s = 56.67
+print(t+s)
