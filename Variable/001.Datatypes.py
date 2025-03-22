b036b0deefa44a27cfea6b2909f54ee8a25010d2
Arvinder Pal <54645908+Arvinderpal10@users.noreply.github.com>
2025-03-22 15:43:22 +0530

    Operators in Python

---
+++ Variable/001.Datatypes.py
@@ -0,0 +1,50 @@
+x = 12
+y = 123.45
+
+v = "123"
+flag = True
+
+#  type() -- It tells us about the type of the variable
+
+print(type(x))   # int 
+
+print(x+y)
+
+# Type Coversion / Type casting 
+print(x+ int(v))
+
+# Can we convert all types of string
+g = "Govind123"
+# print(x+ int(g))
+
+#  Bool -- Int
+
+print(True)
+print(40)
+print(int(True))
+print(int(False))
+
+print(int(True) -int(False) )
+
+print(int(bool(100)))
+
+# Float to int || int to float
+f = 234.56
+e = 23
+print(f*5)
+print(int(f)*5)
+print(f*5 - int(f)*5)
+print(int(f))
+
+print(float(e))
+
+
+
+
+
+
+
+
+
+
+
---
+++ Variable/002.Operators.py
@@ -0,0 +1,79 @@
+#Operators -- 
+
+# Arithmetic Operators 
+a = 100
+b = 300
+c = 50.5
+print(a+b)
+print(b+c)
+print(a-b)
+print(b-c)
+
+print(a*b)
+print(a*c)
+
+print(a/b)
+print(c/a)
+print(b//a)
+print(c//a)
+
+print(3*4+5)
+print(3*(4+5))
+#Modulus Operator
+print(5%2)
+print(50.5%2)
+
+#  Above -- They give us a value 
+# Comparision --- True / False. - Boolean 
+print(5>7)
+print(5<7)
+print(4==5)
+#  less than equla to 
+print(3<=7)
+print(4 >=5)
+
+# Assignment Operator 
+#  +=
+x = 50
+y = 40
+x+= 10  # x = x + 10
+print(x)
+y-=30
+print(y)
+y*=5
+print(y)
+y/=10
+print(y)
+
+# Binary -- Bi - Two - 1 0 -- 
+
+# 0001 --- 1
+# 0010 -- 2 
+# 0011  --3
+# 0100 - 4
+# 0101 - 5
+# 0110 --6
+# 0111 - 7
+# 1000 -- 8
+# 1001 -9
+# 1010 - 10
+
+"""
+0       0       0       0
+2^2   2^2    2^1     2^0
+4+2
+0     1       1         0
+
+14 - 8 + 4+2
+
+24 - 16 + 8 
+11000
+1110
+"""
+
+# import math
+# print(int(math.pow(3,2)))
+# Bit wise operator 
+
+# Logical Operator 
+# Pointers 
