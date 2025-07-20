bceb92dc36adcd80e61206862264f9118f332a44
govindiyer <raminantstump@gmail.com>
2025-07-20 14:35:24 +0530

    Python Questions

--- Practice/SetsHW.py
+++ Practice/SetsHW.py
@@ -13,6 +13,9 @@ Next Questions :
 """
 
 
+
+
+#1
 n=int(input("Enter a number"))
 i=0
 while n>0:
@@ -21,3 +24,16 @@ while n>0:
         max=j
     num=num//10
 print("The maximum digit in the number is,",i)
+
+#2
+
+l = [1, 5, 3, 5, 7, 9, 5, 2, 5]
+n=int(input("Enter a number from the given List above"))
+c = 0
+
+for i in l:
+    if i == n:
+        c += 1
+
+print(f"The number {n} appears {c} times in the list.")
+
