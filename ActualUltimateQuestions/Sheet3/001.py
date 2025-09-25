a5e7d29bdf662164e6cb76cc0e0e2905a5be6877
govindiyer <raminantstump@gmail.com>
2025-09-25 09:29:52 +0530

    Python

---
+++ ActualUltimateQuestions/Sheet3/001.py
@@ -0,0 +1,52 @@
+# How can you print numbers from 1 to 20 using a for loop?
+"""
+for i in range(1,21):
+    print(i)
+"""
+# What loop can display numbers from 10 to 1 in descending order?
+"""
+for i in range(10,0,-1):
+    print(i)
+"""
+# How do you print even numbers between 1 and 30 using a for loop?
+"""
+for i in range(2,31,2):
+    print(i)
+"""
+# What code prints odd numbers from 5 to 25?
+"""
+for i in range(5,26,2):
+    print(i)
+"""
+# How can you print the first 10 multiples of 3?
+"""
+n=3
+for i in range(1,11):
+    print(f"{3} multiplied with {i} is {n*i}")
+"""
+# What for loop prints numbers from 1 to 50, skipping multiples of 5?
+"""
+for i in range(1,51):
+    if i%5==0:
+        continue
+    print(i)
+"""
+# How do you print the squares of numbers from 1 to 10?
+"""
+for i in range(1,11):
+    print(f"The square of {i} is {i**2}")
+"""
+# What loop displays numbers from 100 to 50 in steps of 5?
+"""
+for i in range(100,49,-5):
+    print(i)
+"""
+# How can you print numbers divisible by 4 between 1 and 40?
+"""
+for i in range(1,41):
+    if i%4==0:
+        print(i)
+"""
+# What code prints the first 15 numbers in steps of 2 starting from 0?
+for i in range(0,31,2):
+    print(i)
