2424b56ce81cda7236b391397b9828d8643c96f6
govindiyer <raminantstump@gmail.com>
2025-10-03 10:04:32 +0530

    Python

--- ActualUltimateQuestions/Sheet3/005.py
+++ ActualUltimateQuestions/Sheet3/005.py
@@ -26,16 +26,56 @@ while password:
         print("Incorrect password,Try another password")
 """
 # What loop prints numbers from 1 to 50 that are divisible by 9?
+"""
 for i in range(9,51,9):
     print(i)
+"""
 # How do you print a countdown from n to 1, where n is user input?
-
+"""
+n=int(input("Enter a number:  "))
+for i in range(n,0,-1):
+    print(i)
+"""
 # What while loop prints numbers until their sum exceeds 100?
-
-# How can you print the digits of a number one per line?
-
+"""
+n=1
+s=0
+while s<=100:
+    print(f"{n}")
+    s+=n
+    n+=1
+print(f"Final sum: {s}")
+"""
+# How can you print the digits of a number one per line
+"""
+n=1234567890
+d=[]
+while n>0:
+    i=n%10
+    d.append(i)
+    n//=10
+x=len(d)-1
+while x>=0:
+    print(d[x])
+    x-=1
+"""
 # What code finds the number of iterations until a number becomes 1?
-
+"""
+n=int(input("Enter a number:  "))
+i=0
+while n!=1:
+    if n%2==0:
+        n=n//2
+    else:
+        n=(n*3)+1
+    i+=1
+print(i)
+"""
 # (divide by 2 if even, multiply by 3 and add 1 if odd)?
 
 # How do you print numbers from 1 to 10 with their factorials?
+n=int(input("Enter a number:  "))
+x=1
+for i in range(1,n+1):
+    x*=i
+print(x)
