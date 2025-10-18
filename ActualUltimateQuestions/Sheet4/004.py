696732bd81818c11d2f35cd5b74f3554a55c2781
govindiyer <raminantstump@gmail.com>
2025-10-18 09:57:47 +0530

    Python

--- ActualUltimateQuestions/Sheet4/004.py
+++ ActualUltimateQuestions/Sheet4/004.py
@@ -7,21 +7,73 @@ def square(n):
 print(square(n))
 """
 # What function returns the sum of two numbers passed as parameters?
-def sums(a,b):
-    x=a+b
-    return x
+"""
+def sup(a,b):
+    result=a+b
+    return result
+print(sup(67,83))
+"""
 # How can a function return the factorial of a given number?
-
+"""
+x=int(input("Enter a number:  "))
+def factorial():
+    factorial=1
+    for i in range(1,x+1):
+        factorial=factorial*i
+    return factorial
+n=factorial()
+print(n)
+"""
 # What function returns the maximum of three numbers?
-
+"""
+def maximum(a,b,c):
+    result=max(a,b,c)
+    return result
+print(maximum(15,22,42))
+"""
 # How do you create a function to return the number of vowels in a string?
-
+"""
+s=str(input("Enter a word:  "))
+def vowel():
+    count=0
+    for i in s:
+        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+            count+=1
+    return count
+print(vowel())
+"""
 # What function returns the reverse of a number?
+"""
 
+"""
 # How can a function return the area of a circle given the radius?
-
+"""
+def area(a):
+    result=3.14*a*a
+    return result
+print(area(5))
+"""
 # What function returns true if a number is even, false otherwise?
-
+"""
+def evenodd(a):
+    if a%2==0:
+        result=(bool(1))
+    else:
+        result=(bool(0))
+    return result
+print(evenodd(int(input("Enter a number:  "))))
+"""
 # How do you write a function to return the sum of digits in a number?
-
+"""
+def digitsum(a):
+    for i in a:
+        print(sum(a))
+    return i
+print(digitsum([1,2,3,4,5,6,7,8,9]))
+"""
 # What function returns the length of a string passed as input?
+def length(a):
+    for i in a:
+        result=len(a)
+    return result
+print(length("Govind"))
---
+++ ActualUltimateQuestions/Sheet4/005.py
@@ -0,0 +1,53 @@
+# How can a function return the product of two numbers?
+"""
+def product(a,b):
+    result=a*b
+    return result
+print(product(5,10))
+"""
+# What function returns true if a number is prime?
+"""
+def prime(a):
+    for i in range(1,a):
+        if 0:
+            result=(bool(1))
+        else:
+            result=(bool(0))
+    return result
+print(prime(15))
+"""
+# How do you write a function to return the Celsius to Fahrenheit conversion?
+"""
+def celsius(a):
+    farenheight=a*1.8+32
+    return farenheight
+print(celsius(int(input("Enter a number:  "))))
+"""
+# What function returns the count of characters in a string excluding spaces?
+"""
+def characters(a):
+    count=0
+    for i in a:
+        count+=1
+    return count
+print(characters(input("Enter a word:  ")))
+"""
+# How can a function return the average of a list of numbers?
+"""
+def avg(li):
+    res=0
+    for i in li:
+        res+=i
+    average=res/len(i)
+    return average
+print(avg([1,2,3,4,5,6,7,8,9]))
+""" 
+# How do you write a recursive function to calculate the factorial of a number?
+
+# What recursive function returns the nth Fibonacci number?
+
+# How can a recursive function print numbers from n to 1?
+
+# What recursive function calculates the sum of numbers from 1 to n?
+
+# How do you write a recursive function to reverse a string?
